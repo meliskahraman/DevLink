a95f921a06d231957bb7aa90d5c032f42b17b6ba
meliskahraman <melis.2004k@gmail.com>
2026-05-13 20:46:31 +0300

    Add login register switch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,24 @@
+import { useState } from "react";
 import LoginForm from "./components/LoginForm";
+import RegisterForm from "./components/RegisterForm";
 
 function App() {
-  return <LoginForm />;
+  const [mode, setMode] = useState("login");
+
+  return (
+    <div style={{ padding: "40px" }}>
+      <h1>DevLink</h1>
+
+      <button onClick={() => setMode("login")}>Login</button>
+      <button onClick={() => setMode("register")} style={{ marginLeft: "10px" }}>
+        Register
+      </button>
+
+      <hr />
+
+      {mode === "login" ? <LoginForm /> : <RegisterForm />}
+    </div>
+  );
 }
 
 export default App;
